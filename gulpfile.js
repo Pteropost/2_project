a9f7c459a7714cbae6ea5aabf7da350763887028
Papko Valdislav <Pteropost@gmail.com>
2022-02-08 21:58:26 +0300

    For Site

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,65 @@
 const {src, dest, parallel, series, watch} = require('gulp');
 const browserSync = require('browser-sync');
-// const autoprefixer = require('gulp-autoprefixer');
-// const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 const del = require('del');
-// const image = require('gulp-image');
-const imagemin = require('gulp-imagemin')
+const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
-
-// function browsersync() {
-//     browserSync.init({
-//         server: {
-//             baseDir: 'build'
-//         }
-//     })
-// }
+const pug = require("gulp-pug");
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: 'build'
+        }
+    })
+}
 
 function html() {
-    return src('src/index.html')
+    return src('src/index.pug')
+        .pipe(pug({pretty: true}))
         .pipe(dest('build'))
-        // .pipe(browserSync.stream())
+        .pipe(browserSync.stream())
 }
 
 function css() {
     return src('src/assets/styles/styles.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         // .pipe(autoprefixer({
-//         //     overrideBrowserslist: ['last 2 versions'],
-//         //     grid: 'autoplace',
-//         // }))
-//         // .pipe(cleanCSS())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            grid: 'autoplace',
+        }))
+        .pipe(cleanCSS())
         .pipe(dest('build/assets/styles'))
-//         .pipe(browserSync.stream())
+        .pipe(browserSync.stream())
 }
 
 function images() {
     return src('src/assets/images/**/*')
-        // .pipe(image())
         .pipe(imagemin())
         .pipe(dest('build/assets/images'))
-        // .pipe(browserSync.stream())
+        .pipe(browserSync.stream())
 }
 
 function fonts() {
     return src('src/assets/fonts/**/*')
-         .pipe(dest('build/assets/fonts'))
-        // .pipe(browserSync.stream())
+        .pipe(dest('build/assets/fonts'))
+        .pipe(browserSync.stream())
 }
 
 function clear() {
-     return del('build', {force: true});
- }
+    return del('build', {force: true});
+}
 
 
-// function startWatch() {
-//     watch('src/index.html', html)
-//     watch('src/assets/styles/**/*.scss', css)
-//     watch('src/assets/images/**/*', images)
-//     watch('src/assets/fonts/**/*', fonts)
-// }
+function startWatch() {
+    watch('src/**/*.pug', html)
+    watch('src/assets/styles/**/*.scss', css)
+    watch('src/assets/images/**/*', images)
+    watch('src/assets/fonts/**/*', fonts)
+}
 
-exports.dev = parallel(html, images, fonts, css )
-exports.build = series(clear, parallel(html, images, fonts, css))
+exports.dev = parallel(browsersync, startWatch, html, css, images, fonts)
+exports.build = series(clear, parallel(html, css, images, fonts))
 
 
-exports.default = series(html, html, images, fonts, css)
+exports.default = parallel(browsersync, startWatch, html, css, images, fonts)
